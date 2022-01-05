e984ef17ce91456a089e86bb784dd222c7e03f36
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2022-01-05 23:11:51 +0700

    Add verify token middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const authHeader = req.header('Authorization');
+    const accessToken = authHeader && authHeader.split(" ")[1]; // 'Authorization': 'Bearer alonglongstring'
+
+    if (!accessToken) {
+        return res.status(401).json({
+            success: false,
+            message: "Access token not found"
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+        req.userId = decoded.userId;
+        next();
+    } catch (error) {
+        return res.status(403).json({
+            success: false,
+            message: "Invalid token"
+        });
+    }
+}
+
+module.exports = verifyToken;
+
+
+
+
