d8d30e8ad1d285497f387d27a360bca808964b05
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2022-01-05 23:13:27 +0700

    Add routes

---
+++ routes/todo.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+
+/**
+ * @route GET api/v1/todos
+ * @desc  Get all todos
+ * @access Private
+ */
+router.get('/', async (req, res) => {
+
+});
+
+/**
+ * @route GET api/v1/todos/:id
+ * @desc  Get a todo
+ * @access Private
+ */
+router.get('/:id', async (req, res) => {
+
+});
+
+/**
+ * @route POST api/v1/todos
+ * @desc  Add a todo
+ * @access Private
+ */
+router.post('/', async (req, res) => {
+
+});
+
+/**
+ * @route PUT api/v1/todos/:id
+ * @desc  Get all todos
+ * @access Private
+ */
+router.put('/:id', async (req, res) => {
+
+});
+
+/**
+ * @route DELETE api/v1/todos/:id
+ * @desc  Get all todos
+ * @access Private
+ */
+router.delete('/:id', async (req, res) => {
+
+});
+
+module.exports = router;
