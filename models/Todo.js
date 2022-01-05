83d56ce5694b2630660234a100b5b93a497aa718
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2022-01-05 23:12:28 +0700

    Add todo model

---
+++ models/Todo.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    desc: {
+        type: String,
+        required: true,
+    },
+    isCompleted : {
+        type: Boolean,
+        default: false
+    },
+    createdAt : {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+module.exports = mongoose.model("todos", TodoSchema);
+
+
+
