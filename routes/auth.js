f4c0521df6af83b122c89241170c6225e26caffd
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2022-01-06 17:33:34 +0700

    Add validators to auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,143 +1,52 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-
-const User = require('../models/User');
+const authController = require('../controllers/auth.controller')
 const verifyToken = require('../middlewares/auth');
+const { check } = require('express-validator');
 
 /**
  * @route GET api/v1/auth
  * @desc Check authentication status
  * @access Public
  */
-router.get('/', verifyToken, async (req, res) => {
-    const userId = req.userId;
-    try {
-        const user = await User.findById(userId).select("-password");
-        if (!user) {
-            return res.status(400).json({
-                success: false,
-                message: "User not found"
-            });
-        }
-
-        return res.json({
-            success: true,
-            user
-        })
-
-    } catch (error) {
-        return res.status(500).json({
-            success: false,
-            message: "Internal server error"
-        });
-    }
-
-});
+router.get('/',
+    verifyToken,
+    authController.checkAuthStatus);
 
 /**
  * @route POST api/v1/auth/register
  * @desc Register a new user
  * @access Public
  */
-router.post('/register', async (req, res) => {
-    const { username, password } = req.body;
-
-    // simple validation
-    if (!username || !password) {
-        return res.status(400).json({
-            success: false,
-            message: "Missing username or password"
-        });
-    }
-
-    try {
-        const user = await User.findOne({ username });
-
-        if (user) {
-            return res.status(400).json({
-                success: false,
-                message: "Username has already been taken"
-            })
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 11);
-        const newUser = new User({
-            username,
-            password: hashedPassword
-        });
-
-        await newUser.save();
-
-        const accessToken = jwt.sign({
-            userId: newUser._id
-        },
-            process.env.ACCESS_TOKEN_SECRET);
-
-        return res.json({
-            success: true,
-            message: "Successfully registered",
-            accessToken
-        })
-
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success: false,
-            message: "Internal server error"
-        })
-    }
-})
-
+router.post('/register',
+    [
+        check("username")
+            .isLength({ min: 6 })
+            .withMessage("Username must be at leat 5 characters long")
+            .trim()
+        ,
+        check("password")
+            .isLength({ min: 6 })
+            .withMessage("Password must be at leat 5 characters long")
+            .trim()
+    ],
+    authController.register);
 
 /**
  * @route POST api/v1/auth/login
  * @des Login a user
  * @access Public
  */
-router.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-
-    try {
-        const user = await User.findOne({ username });
-
-        if (!user) {
-            return res.status(400).json({
-                success: false,
-                message: "Invalid credentials"
-            });
-        }
-
-        const passwordMatch = await bcrypt.compare(password, user.password);
-
-        if (!passwordMatch) {
-            return res.status(400).json({
-                success: false,
-                message: "Invalid credentials"
-            });
-        }
-
-        const accessToken = jwt.sign({
-            userId: user._id
-        },
-            process.env.ACCESS_TOKEN_SECRET);
-
-
-        return res.json({
-            success: true,
-            message: "Successfully login",
-            accessToken
-        });
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({
-            success: false,
-            message: "Internal server error"
-        })
-    }
-
-})
+router.post('/login',
+    [
+        check("username")
+            .isLength({ min: 6 })
+            .withMessage("Username must be at leat 5 characters long")
+        ,
+        check("password")
+            .isLength({ min: 6 })
+            .withMessage("Password must be at leat 5 characters long")
+    ]
+    , authController.login);
 
 module.exports = router;
--- routes/todo.js
+++ routes/todo.js
@@ -1,49 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const todoController = require('../controllers/todo.controller');
+
 
 /**
  * @route GET api/v1/todos
  * @desc  Get all todos
  * @access Private
  */
-router.get('/', async (req, res) => {
-
-});
+router.get('/',
+    todoController.getAllTodos
+);
 
 /**
  * @route GET api/v1/todos/:id
  * @desc  Get a todo
  * @access Private
  */
-router.get('/:id', async (req, res) => {
-
-});
+router.get('/:id',
+    todoController.getTodo
+);
 
 /**
  * @route POST api/v1/todos
  * @desc  Add a todo
  * @access Private
  */
-router.post('/', async (req, res) => {
-
-});
+router.post('/',
+    todoController.addTodo
+);
 
 /**
  * @route PUT api/v1/todos/:id
  * @desc  Get all todos
  * @access Private
  */
-router.put('/:id', async (req, res) => {
-
-});
+router.put('/:id',
+    todoController.updateTodo
+);
 
 /**
  * @route DELETE api/v1/todos/:id
  * @desc  Get all todos
  * @access Private
  */
-router.delete('/:id', async (req, res) => {
-
-});
+router.delete('/:id',
+    todoController.deleteTodo
+);
 
 module.exports = router;
