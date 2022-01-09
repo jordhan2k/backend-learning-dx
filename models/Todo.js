c87cd5014bcc84814e4053faac6c8cca763f4a17
Jordan Hwang <dunghoang1c.hanu@gmail.com>
2022-01-09 22:49:15 +0700

    Add mongoose-paginate plugin

--- models/Todo.js
+++ models/Todo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
 const Schema = mongoose.Schema;
+const mongoosePaginate = require('mongoose-paginate-v2')
 
 const TodoSchema = new Schema({
     title: {
@@ -24,6 +24,8 @@ const TodoSchema = new Schema({
         ref: "users"
     }
 })
+
+TodoSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model("todos", TodoSchema);
 
